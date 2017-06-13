98428b4dcc92081b339e523dde7636e3c1430aef
Wojtek Pietrucha <wojtekpietrucha@gmail.com>
2017-06-13 19:25:10 +0200

    Example - Spys in mockito.

---
+++ src/test/java/SimpleSpyTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class SimpleSpyTest {
+
+    @Test
+    public void testSpys(){
+        List list = new LinkedList();
+        List spy = Mockito.spy(list);
+
+        //optionally, you can stub out some methods:
+        when(spy.size()).thenReturn(100);
+
+        //using the spy calls *real* methods
+        spy.add("one");
+        spy.add("two");
+
+        assertThat(spy.get(0)).isEqualTo("one");
+        assertThat(spy.get(1)).isEqualTo("two");
+        verify(spy).add("one");
+        verify(spy).add("two");
+
+        assertThat(spy.size()).isEqualTo(100);
+
+        //optionally, you can verify
+    }
+}
